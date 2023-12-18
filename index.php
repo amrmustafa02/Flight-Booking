45313a9fca99961bf78c974aa4a578d7b351223a
amrmustafa6051 <amrmustafa6051@gmail.com>
2023-12-18 23:20:57 +0200

    init app

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<script>
+    function goToLogin() {
+        window.location.href = 'features/login/login_ui.php';
+    }
+</script>
+
+<body>
+    <button onclick="goToLogin()">
+        go to login
+    </button>
+</body>
+
+</html>
