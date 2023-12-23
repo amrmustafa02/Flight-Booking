b3c4e9ba69ff8cc0b8d16b8b155fb6ced7c17b41
youssefElkilany <elkilany17@gmail.com>
2023-12-23 23:24:54 +0200

    register php

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbName = 'flight';
+
+// //$db =  mysqli_connect($host,$user,$pass,$dbName);
+$conn = new mysqli($host,$user,$pass,$dbName) or die("unable to connect");
+
+// echo "gg";
+// echo "php is under control"
+
+?>
--- features/register/register.back.php
+++ features/register/register.back.php
@@ -1,11 +1,48 @@
 <?php
 $err = "";
 // Check if the form is submitted
+echo "saa";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include("../../connection.php");
     $email = $_POST['email'];
     $name = $_POST['name'];
     $password = $_POST['password'];
-    $hashed_password = md5($password);
+    //$hashed_password = md5($password);
+
+   
+    $sql = "select * from company , passenger where email = '$email'";
+$result = mysqli_query($conn,$sql);
+$count_email = mysqli_num_rows($result);
+
+if($count_email != 0)
+{
+    $err = "email already exist";
+}
+if($password != $cpassword)
+{
+    $err = "password dont match";
+}
+
+$hashedpass = password_hash($password,PASSWORD_DEFAULT);
+
+
+if($type == 'company')
+{
+    $sql = "insert into company(username,email,password,tel,type) values('$username','$email','$tel','$type')";
+    $result = mysqli_query($conn,$sql);
+}
+else if($type == 'passenger')
+{
+    $sql = "insert into passenger(username,email,password,tel,type) values('$username','$email','$tel','$type')";
+    $result = mysqli_query($conn,$sql);
+
+}
+// if($result){
+//     header("Location: login.php"); // msh 3arf ht7tago wla l2 
+// }
+
+
 
     // TODO:add new user in database
 
