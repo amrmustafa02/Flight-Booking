ed110a7394d2a5c501d82995c2fdcced91813610
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-12-24 21:25:22 +0200

    some updates

--- features/complete-info-company/info.back.php
+++ features/complete-info-company/info.back.php
@@ -1,14 +1,32 @@
 <?php
+$err = "";
+session_start();
+include "../../connection.php";
+require_once '../../constant.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve data from the form using the $_POST superglobal
-
-    $userName = isset($_POST['userName']) ? $_POST['userName'] : "";
+    $username = isset($_POST['username']) ? $_POST['username'] : "";
     $bio = isset($_POST['bio']) ? $_POST['bio'] : "";
     $address = isset($_POST['address']) ? $_POST['address'] : "";
     $location = isset($_POST['location']) ? $_POST['location'] : "";
-//    $logo = isset($_FILES['logo']) ? $_FILES['logo']["name"] : "";
- // TODO:take logo
 
+    // Get the ID of the recently inserted record
+    $companyID = UserData::$userId;
 
+    echo "<p>$companyID </p>"; // Move this line here
+
+    $updateQuery = "UPDATE company SET username = '$username', bio = '$bio', address = '$address', location = '$location' WHERE companyId = $companyID";
+    $updateResult = mysqli_query($conn, $updateQuery);
+
+    // if ($updateResult) {
+    //     echo "Record updated successfully!";
+    //     unset($_SESSION['companyID']);
+    // } else {
+    //     // Handle update failure
+    //     echo "Error updating record: " . mysqli_error($conn);
+    // }
+
+    // $logo = isset($_FILES['logo']) ? $_FILES['logo']["name"] : "";
+    // TODO: take logo
 }
 ?>
--- features/register/register.back.php
+++ features/register/register.back.php
@@ -1,51 +1,72 @@
 <?php
 $err = "";
-// Check if the form is submitted
 include "../../connection.php";
+require_once '../../constant.php';
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    
     $email = $_POST['email'];
     $name = $_POST['name'];
     $password = $_POST['password'];
     $tel = $_POST['tel'];
     $userType = $_POST['userType'];
-    
 
-    
-    if($userType == "company")
-    {
-        $sql = "select * from company where email = '$email'";
+    // Check if the email already exists
+    if ($userType == "company") {
+        $sql = "SELECT * FROM company WHERE email = '$email'";
+    } else {
+        $sql = "SELECT * FROM passenger WHERE email = '$email'";
     }
-    else
-    {
-        $sql = "select * from passenger where email = '$email'";
-    }
-    
+
     $result = mysqli_query($conn, $sql);
     $count_email = mysqli_num_rows($result);
 
     if ($count_email != 0) {
-        $err = "email already exist";
+        $err = "Email already exists";
+        echo "<p>$err</p>";
+        exit;
     }
 
-    $hashedpass = password_hash($password, PASSWORD_DEFAULT);
+    // Hash the password
+    $hashedpass=md5($password);
 
-   
+    // Perform the insertion
     if ($userType == 'company') {
-        echo "insert";
-        $sql = "insert into company(name,email,password,tel,userType) values('$name','$email','$hashedpass','$tel','$userType')";
+        // $hu = "hello:";
+        // echo "<p>$hu</p>";
+        $sql = "INSERT INTO company (name, email, password, tel, userType) VALUES ('$name', '$email', '$hashedpass', '$tel', '$userType')";
+        // $hu = "hello:";
+        // echo "<p>$hu</p>";
         $result = mysqli_query($conn, $sql);
-    } else if ($userType == 'passenger') {
-        $sql = "insert into passenger(name,email,password,tel,userType) values('$name','$email','$hashedpass','$tel','$userType')";
-        $result = mysqli_query($conn, $sql);
+        // $hu = "hello:";
+        // echo "<p>$hu</p>";
+        if ($result) {
+            // Retrieve the last inserted company ID
+            // $hu = "hello:";
+            // echo "<p>$hu</p>";
+            // echo "hello: " ;
+            $result2 = mysqli_query($conn, "SELECT companyId FROM company ORDER BY companyId DESC LIMIT 1");
 
+            if ($result2 && mysqli_num_rows($result2) > 0) {
+                // $hu = "hello:";
+                // echo "<p>$hu</p>";
+                $row = mysqli_fetch_assoc($result2);
+                $companyID = $row['companyId'];
+                UserData::$userId = $companyID;
+                echo UserData::$userId;
+                // Set the company ID in the session
+                // $_SESSION['companyId'] = $companyID;
+                // echo "Company ID: " . $_SESSION['companyId'];
+
+                // Redirect to the home screen
+                // header("Location: ../../index.php");
+            }
+        }
+    } else if ($userType == 'passenger') {
+        $sql = "INSERT INTO passenger (name, email, password, tel, userType) VALUES ('$name', '$email', '$hashedpass', '$tel', '$userType')";
+        $result = mysqli_query($conn, $sql);
     }
 
-
-  
-
-   
-
+    // TODO: Redirect to the home screen or handle as needed
 }
+?>
