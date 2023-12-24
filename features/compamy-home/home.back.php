b6b1acb6d93834eab771529fc860294bceb755f4
amrmustafa02 <amrmustafa6051@gmail.com>
2023-12-24 14:13:28 +0200

    edit in home screen

---
+++ features/compamy-home/home.back.php
@@ -0,0 +1,2 @@
+<?php
+ echo "<p>hi</p>";
---
+++ features/compamy-home/home.ui.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Company Home</title>
+</head>
+<body>
+<div class="buttons-div">
+    <div class="company-info">
+        <span class="company-name">Your Company Name</span>
+    </div>
+    <button class="home-button">Add Flight</button>
+    <button class="home-button">Messages</button>
+    <button class="home-button">Profile</button>
+</div>
+</body>
+</html>
