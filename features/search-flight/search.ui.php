51b8bd87e99da01110a8ccf21753de1405a99551
amrmustafa02 <amrmustafa6051@gmail.com>
2023-12-25 06:10:54 +0200

    finish search screen

--- features/search-flight/search.ui.php
+++ features/search-flight/search.ui.php
@@ -2,7 +2,34 @@
 <head>
 
     <title> Search Flight</title>
+    <link rel="stylesheet" href="search.css">
+
 </head>
 
+<body>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <div class="input-group">
+        <div class="input-pair">
+            <label style="color: black" for="fromCity">From City</label>
+            <input type="text" id="fromCity" name="fromCity" required>
+        </div>
+        <div class="input-pair">
+            <label for="toCity">To City</label>
+            <input type="text" id="toCity" name="toCity" required>
+        </div>
+    </div>
+    <div class="search-button" style="text-align: center">
+        <button class="login-button">Search</button>
+    </div>
+</form>
+
+
+<?php
+ include "search.back.php";
+?>
+
+
+</body>
 
 </html>
