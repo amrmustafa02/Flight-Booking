407a315af3d6ba4707b2491a514f7471af0a4477
youssefElkilany <elkilany17@gmail.com>
2023-12-25 19:35:01 +0200

    edit profile and show details of profile and messages

--- features/register/register.back.php
+++ features/register/register.back.php
@@ -56,8 +56,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($result2 && mysqli_num_rows($result2) > 0) {
                 $row = mysqli_fetch_assoc($result2);
                 $passengerID = $row['passengerId'];
-                UserData::$userId = $passengerID;
-                echo UserData::$userId;
+                // UserData::$userId = $passengerID;
+                // echo UserData::$userId;
                 header("Location: ../complete-info-passenger/info.ui.php?id=" . urlencode($passengerID));
                 exit();
             }
