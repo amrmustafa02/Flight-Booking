dc72490845521e98024163c0250abe40f3dfd0b8
amrmustafa02 <amrmustafa6051@gmail.com>
2023-12-26 02:32:46 +0200

    fix erorr in add-flight

--- features/add-flight/add.flight.back.php
+++ features/add-flight/add.flight.back.php
@@ -7,31 +7,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Foreign Key from Company 
 
     $companyId = $_COOKIE['id'];
-    $name= $_POST['name'];
-    $Id = $_POST["id"];
+    $name = $_POST['name'];
     $itineraryFrom = $_POST['fromCity'];
     $itineraryTo = $_POST['toCity'];
     $numberOfPassengers = $_POST['numberOfPassengers'];
-    $fees= $_POST['fees'];
-    $start_date= $_POST['startDate'];
-    $end_date= $_POST['endDate'];
-    $start_time= $_POST['startTime'];
-    $end_time= $_POST['endTime'];
+    $fees = $_POST['fees'];
+    $start_date = $_POST['startDate'];
+    $end_date = $_POST['endDate'];
+    $start_time = $_POST['startTime'];
+    $end_time = $_POST['endTime'];
 
 
-    $sql = "INSERT INTO Flight (name,Id,itineraryFrom,itineraryTo,numberOfPassengers,fees,start_date, end_date,start_time,end_time)
-            VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?)";
-    
-    // prepare & execute the query
+    $sql = "INSERT INTO Flight (compId,name, itineraryFrom, itineraryTo, numberOfPassengers, fees, start_date, end_date, start_time, end_time)
+        VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
-   $stmt->bind_param("sissidssss",$name,$Id,$itineraryFrom,$itineraryTo,$numberOfPassengers,$fees,$start_date,$end_date,$start_time,$end_time);
- 
-   if($stmt->execute()){
-        echo "<p>Flight Added Successfully</p>";
-    }
-    else{
-        echo "<p>Error</p>". $stmt->error;
+
+    $stmt->bind_param("sissiissss", $companyId, $name, $itineraryFrom, $itineraryTo, $numberOfPassengers, $fees, $start_date, $end_date, $start_time, $end_time);
+
+    if ($stmt->execute()) {
+        echo "<p style='color: green;font-size: 22px'>Flight Added Successfully</p>";
+    } else {
+        echo "<p>Error</p>" . $stmt->error;
     }
 
     $stmt->close();
@@ -40,5 +37,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 
-?>
+
 
