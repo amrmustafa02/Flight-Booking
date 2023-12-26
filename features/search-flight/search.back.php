97cff82446218a74c5c6afc5d6661df14298eb1f
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-12-26 22:29:58 +0200

    search for flight

--- features/search-flight/search.back.php
+++ features/search-flight/search.back.php
@@ -1,13 +1,36 @@
 <?php
-
+$err = "";
+include "../../connection.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cityFrom = $_POST['fromCity'];
     $cityTo = $_POST['toCity'];
 
 
     //TODO: search in database
-
-
+    $query = "SELECT * FROM flight WHERE itineraryFrom='$cityFrom' AND itineraryTo='$cityTo' AND cancelled=false";
+    $results = mysqli_query($conn, $query);
+    
+    if ($results) {
+        // Check if there are rows in the result set
+        if (mysqli_num_rows($results) > 0) {
+            // Loop through the result set
+            while ($row = mysqli_fetch_assoc($results)) {
+                $name = $row['name'];
+                $time_start=$row['start_time'];
+                $time_end=$row['end_time'];
+                // Echo the flight name
+                echo "<p>Name: $name</p>";
+                echo "<p>time_start: $time_start</p>";
+                echo "<p>time_end: $time_end</p>";
+            }
+        } else {
+            // No matching flights found
+            echo "No matching flights found.";
+        }
+    } else {
+        // Handle query error
+        echo "Error retrieving flight names: " . mysqli_error($conn);
+    }
     // appear ui here
     // Sample array of items
     $listItems = array("Item 1", "Item 2", "Item 3", "Item 4", "Item 5");
